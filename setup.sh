16489fcb12675c5d5b529a8c7d24e655d78e300e
chLi <565353780@qq.com>
2026-01-06 21:41:00 +0800

    add setup bash files

---
+++ setup.sh
@@ -0,0 +1,7 @@
+pip3 install torch torchvision torchaudio \
+  --index-url https://download.pytorch.org/whl/cu128
+
+pip install pillow opencv-python plyfile huggingface_hub \
+  safetensors evo scipy einops tqdm
+
+pip install numpy==1.26.4
